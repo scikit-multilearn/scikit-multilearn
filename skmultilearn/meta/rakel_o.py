b8243492bfee6fa33b81e373d332d52a7b5bb1ba
Piotr Szymański <niedakh@gmail.com>
2014-11-26 21:44:59 +0100

    - working rakel_o

--- skmultilearn/meta/rakel_o.py
+++ skmultilearn/meta/rakel_o.py
@@ -8,15 +8,14 @@ class RakelO(MLClassifierBase):
 
     def __init__(self, classifier = None, models = None, labelset_size = None):
         super(RakelO, self).__init__(classifier)
-        self.models = np.min(models)
+        self.model_count = int(models)
         self.labelset_size = labelset_size
 
     def sample_models(self):
         label_sets = []
         free_labels = xrange(self.label_count)
-        self.model_count = np.ceil(self.label_count/self.labelset_size)
-
-        while len(label_sets) <= self.model_count:
+        
+        while len(label_sets) < self.model_count:
             label_set = random.sample(free_labels, self.labelset_size)
             if label_set not in label_sets:
                 label_sets.append(label_set)
@@ -32,8 +31,8 @@ class RakelO(MLClassifierBase):
         self.label_count = len(y[0])
         self.sample_models()
         for i in xrange(self.model_count):
-            classifier = copy.copy(self.classifier)
-            y_subset = self.subset(y,self.label_sets[i])
+            classifier = copy.deepcopy(self.classifier)
+            y_subset = self.generate_data_subset(y,self.label_sets[i])
             classifier.fit(X,y_subset)
             self.classifiers.append(classifier)
 
@@ -55,15 +54,19 @@ class RakelO(MLClassifierBase):
                 result.append(i)
 
     def predict(self, X):
-        results = []
-        for i in xrange(self.model_count):
-            label_set = np.array(self.label_sets[i])
-            result = [label_set[assigned_labels] for assigned_labels in self.classifiers[i].predict(X)]
-            results.append(result)
+        predictions = [c.predict(X) for c in self.classifiers]
+        votes = np.zeros((len(X),self.label_count), dtype='i8')
+        for row in xrange(len(X)):
+            for model in xrange(self.model_count):
+                for label in xrange(len(self.label_sets[model])):
+                    votes[row,self.label_sets[model][label]] += predictions[model][row][label]
 
-        row_results = []
-        for row in len(X):
-            row_result = [results[i][row] for i in xrange(self.model_count)]
-            row_results.append(row_result)
+        voters = np.zeros(self.label_count, dtype='i8')
+        for label_set in self.label_sets:
+            voters[label_set] += 1
 
-        return row_results
+        for row in xrange(len(X)):
+            for label in xrange(self.label_count):
+                votes[row, label] = int(float(votes[row, label]) / float(voters[label]) > 0.5)
+
+        return votes
