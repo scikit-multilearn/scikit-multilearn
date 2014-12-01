c4b10964c936e0d24a974e14f70ab949f65c453e
Piotr Szymański <niedakh@gmail.com>
2014-12-01 16:06:47 +0100

    - add data

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from distutils.core import setup
 setup(
     name='scikit-multilearn',
     version='0.0.1',
-    packages=['skmultilearn','meka','data'],
+    packages=['skmultilearn','meka', 'data'],
     author=u'Piotr Szymański',
     author_email='niedakh@gmail.com',
     license='BSD',
     long_description=open('README.md').read(),
     url='http://scikit-multilearn.github.io/',
     description= 'A set of python modules for multi-label classification',
+    package_data = {'data': ['*.dump.bz2']},
+    include_package_data = True,
     classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
