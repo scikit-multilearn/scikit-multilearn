36723fb9d44985a094d18cc435b66f06879b62da
Piotr Szymański <niedakh@gmail.com>
2015-02-17 13:12:31 +0100

    - add mlknn

---
+++ skmultilearn/lazy/mlknn.py
@@ -0,0 +1,57 @@
+from ..base import MLClassifierBase
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+class KNearestNeighbours(MLClassifierBase):
+    """k Nearest Neighbours multi-label classifier."""
+
+    def __init__(self, classifier = None, k = 10, s = 1.0):
+        super(KNearestNeighbours, self).__init__(classifier)
+        self.k = k # Number of neighbours
+        self.s = s # Smooth parameter
+
+    def compute_prior(self, y):
+        prior_prob_true = []
+        prior_prob_false = []
+        for label in range(0, self.num_labels):
+            prior_prob_true.append(float(self.s + sum(instance[label] == 1 for instance in y)) / (self.s * 2 + self.num_instances))
+            prior_prob_false.append(1 - prior_prob_true[-1])
+        return prior_prob_true, prior_prob_false
+
+    def compute_cond(self, X, y):
+        self.knn = NearestNeighbors(self.k).fit(X)
+        c = [[0] * (self.k + 1) for label in range(0, self.num_labels)]
+        cn = [[0] * (self.k + 1) for label in range(0, self.num_labels)]
+        for instance in range(0, self.num_instances):
+            neighbors, _ = self.knn.kneighbors(X[instance], self.k)
+            for label in range(0, self.num_labels):
+                delta = sum(n == 1 for n in neighbors[0])
+                (c if y[instance][label] == 1 else cn)[label][delta] += 1
+
+        cond_prob_true = [[0] * (self.k + 1) for label in range(0, self.num_labels)]
+        cond_prob_false = [[0] * (self.k + 1) for label in range(0, self.num_labels)]
+        for label in range(0, self.num_labels):
+            for neighbor in range(0, self.k):
+                cond_prob_true[label][neighbor] = (self.s + c[label][neighbor]) / (self.s * (self.k + 1) + sum(c[label]))
+                cond_prob_false[label][neighbor] = (self.s + cn[label][neighbor]) / (self.s * (self.k + 1) + sum(cn[label]))
+        return cond_prob_true, cond_prob_false
+
+    def fit(self, X, y):
+        self.num_labels = len(y[0])
+        self.num_instances = len(y)
+        # Computing the prior probabilities
+        self.prior_prob_true, self.prior_prob_false = self.compute_prior(y)
+        # Computing the posterior probabilities
+        self.cond_prob_true, self.cond_prob_false = self.compute_cond(X, y)
+
+    def predict(self, X):
+        result = np.zeros((len(X), self.num_labels))
+        for instance in range(0, len(X)):
+            neighbors, _ = self.knn.kneighbors(X[instance], self.k)
+            for label in range(0, self.num_labels):
+                delta = sum(n == 1 for n in neighbors[0])
+                p_true = self.prior_prob_true[label] * self.cond_prob_true[label][delta]
+                p_false = self.prior_prob_false[label] * self.cond_prob_false[label][delta]
+                prediction = (p_true >= p_false)
+                result[instance][label] = int(prediction)
+        return result
