5b0723a812f364539407022cc9e0886c3f4860b8
Piotr Szymański <niedakh@gmail.com>
2016-02-22 18:13:46 +0100

    port b1871a718973bd60a4bcded242f7a9d42e2f3ca2 from ChristianSch/scikit-multilearn add require_dense support closes #24

--- skmultilearn/base.py
+++ skmultilearn/base.py
@@ -92,6 +92,48 @@ class MLClassifierBase(object):
         """
         raise NotImplementedError("MLClassifierBase::predict()")
 
+    def get_params(self, deep=True):
+        """
+        Introspection of classifier for search models like cross validation and grid
+        search.
+        Parameters
+        ----------
+        deep : boolean
+            If true all params will be introspected also and appended to the output dict.
+        Returns
+        -------
+        out : dictionary
+            Dictionary of all parameters and their values. If deep=True the dictionary
+            also holds the parameters of the parameters.
+        """
+        out = dict()
+
+        out["classifier"] = self.classifier
+        out["require_dense"] = self.require_dense
+
+        # deep introspection of estimator parameters
+        if deep and hasattr(self.classifier, 'get_params'):
+            deep_items = value.get_params().items()
+            out.update((key + '__' + k, val) for k, val in deep_items)
+
+        return out
+
+    def set_params(self, **parameters):
+        """
+        Set parameters as returned by `get_params`.
+        @see https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/base.py#L243
+        """
+
+        if not parameters:
+            return self
+
+        for parameter, value in parameters.items():
+            self.setattr(parameter, value)
+
+        return self
+    
+
+
 class RepeatClassifier(MLClassifierBase):
 
     def __init__(self, value_to_repeat = None):
