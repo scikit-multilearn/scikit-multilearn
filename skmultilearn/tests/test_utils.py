a3b4d9b22279c7fc8cea05caa50ecb3604e9e527
Piotr Szymański <niedakh@gmail.com>
2016-02-23 23:44:01 +0100

    created matrix manipulation helpers and tests

---
+++ skmultilearn/tests/test_utils.py
@@ -0,0 +1,38 @@
+import unittest
+import numpy as np
+import scipy.sparse as sp
+
+from ..utils import get_matrix_in_format, matrix_creation_function_for_format
+
+class UtilsTest(unittest.TestCase):
+    SPARSE_MATRIX_FORMATS = ["bsr", "coo", "csc", "csr", "dia", "dok", "lil"]
+
+    def correct_matrix_type(matrix, sparse_format):
+        return 
+
+    def test_if_get_matrix_ensures_type(self):
+        matrix = sp.csr_matrix([])
+        for sparse_format in UtilsTest.SPARSE_MATRIX_FORMATS:
+            new_matrix = get_matrix_in_format(matrix, sparse_format)
+
+            self.assertTrue(sp.issparse(new_matrix))
+            self.assertTrue(new_matrix.format == sparse_format)
+
+    def test_if_matrix_creation_follows_format(self):
+        matrix = np.matrix([])
+        for sparse_format in UtilsTest.SPARSE_MATRIX_FORMATS:
+            new_matrix = matrix_creation_function_for_format(sparse_format)(matrix)
+
+            self.assertTrue(sp.issparse(new_matrix))
+            self.assertTrue(new_matrix.format == sparse_format)
+
+    def test_ensure_get_matrix_does_not_clone_if_format_agrees(self):
+        matrix = np.matrix([])
+        for sparse_format in UtilsTest.SPARSE_MATRIX_FORMATS:
+            created_matrix = matrix_creation_function_for_format(sparse_format)(matrix)
+            converted_matrix = get_matrix_in_format(created_matrix, sparse_format)
+           
+            self.assertTrue(id(created_matrix) == id(converted_matrix))
+
+if __name__ == '__main__':
+    unittest.main()
--- skmultilearn/utils.py
+++ skmultilearn/utils.py
@@ -1,5 +1,24 @@
+import scipy.sparse as sp
+
+
 def get_matrix_in_format(original_matrix, matrix_format):
     if original_matrix.getformat() == matrix_format:
         return original_matrix
     
     return original_matrix.asformat(matrix_format)
+
+def matrix_creation_function_for_format(sparse_format):
+	SPARSE_FORMAT_TO_CONSTRUCTOR = {
+		"bsr": sp.bsr_matrix,
+		"coo": sp.coo_matrix,
+		"csc": sp.csc_matrix,
+		"csr": sp.csr_matrix,
+		"dia": sp.dia_matrix,
+		"dok": sp.dok_matrix,
+		"lil": sp.lil_matrix
+	}
+	
+	if sparse_format not in SPARSE_FORMAT_TO_CONSTRUCTOR:
+		return None
+	
+	return SPARSE_FORMAT_TO_CONSTRUCTOR[sparse_format]
