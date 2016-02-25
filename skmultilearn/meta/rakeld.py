330d1a3ddb0c0e866838dae2b6bda8ec33dbeeab
Piotr Szymański <niedakh@gmail.com>
2016-02-25 15:32:52 +0100

    - port to labelspacepartitionerclassifier

--- skmultilearn/meta/rakeld.py
+++ skmultilearn/meta/rakeld.py
@@ -1,21 +1,20 @@
-from ..base import MLClassifierBase
+from .partition import LabelSpacePartitioningClassifier
 import copy
 import random
 import numpy as np
 from scipy import sparse
 
-class RakelD(MLClassifierBase):
+class RakelD(LabelSpacePartitioningClassifier):
     """Distinct RAndom k-labELsets multi-label classifier."""
 
-    def __init__(self, classifier = None, labelset_size = None):
-        super(RakelD, self).__init__(classifier)
+    def __init__(self, classifier = None, labelset_size = None, require_dense = None):
+        super(RakelD, self).__init__(classifier = classifier, require_dense = require_dense)
         self.labelset_size = labelset_size
 
-    def sample_models(self, label_count = None):
-        if label_count is not None:
-            self.label_count = label_count
-
+    def generate_partition(self, X, y):
         """Internal method for sampling k-labELsets"""
+
+        self.label_count = y.shape[1]
         label_sets = []
         free_labels = xrange(self.label_count)
         self.model_count = int(np.ceil(self.label_count/self.labelset_size))
@@ -31,33 +30,5 @@ class RakelD(MLClassifierBase):
             free_labels = list(set(free_labels).difference(set(label_set)))
             label_sets.append(label_set)
 
-        self.label_sets = label_sets
+        self.partition = label_sets
 
-    def fit_only(self, X, y):
-        """Fit classifier according to X,y, without resampling models."""
-        self.classifiers = []
-        for i in xrange(self.model_count):
-            classifier = copy.deepcopy(self.classifier)
-            y_subset = self.generate_data_subset(y,self.label_sets[i])
-            classifier.fit(X,y_subset)
-            self.classifiers.append(classifier)
-
-        return self
-
-    def fit(self, X, y):
-        """Fit classifier according to X,y, see base method's documentation."""
-        self.sample_models(y.shape[1])
-        return self.fit_only(X, y)
-
-    def predict(self, X):
-        """Predict labels for X, see base method's documentation."""
-        input_rows = X.shape[0]
-        predictions = [self.classifiers[i].predict(X) for i in xrange(self.model_count)]
-        result = sparse.lil_matrix((input_rows, self.label_count), dtype=int)
-
-        for model in xrange(self.model_count):
-            predictions = self.classifiers[model].predict(X).nonzero()
-            for row, column in zip(predictions[0], predictions[1]):
-                result[row, self.label_sets[model][column]] = 1
-
-        return result
