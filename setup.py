4f126076322c02b49935c7aac8769b87ac49f66c
Piotr Szymański <niedakh@gmail.com>
2016-06-03 12:21:36 +0900

    - updating setup.py before release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='scikit-multilearn',
-    version='0.0.1',
+    version='0.0.2',
     packages=['skmultilearn'],
     author=u'Piotr Szymański',
     author_email='niedakh@gmail.com',
@@ -12,8 +12,6 @@ setup(
 #    long_description=open('README.md').read(),
     url='http://scikit-multilearn.github.io/',
     description= 'A set of python modules for multi-label classification',
-    package_data = {'skmultilearn': ['data/*.dump.bz2']},
-    include_package_data = True,
     classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
