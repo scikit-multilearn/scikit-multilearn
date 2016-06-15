10994626a8d3bbeeee36ec2142746edf14072110
Piotr Szymański <niedakh@gmail.com>
2016-06-15 23:46:07 +0200

    update adapt's init.py

--- skmultilearn/adapt/__init__.py
+++ skmultilearn/adapt/__init__.py
@@ -1,2 +1,11 @@
+"""
+The :mod:`skmultilearn.adapt` module implements algorithm
+adaptation approaches to multi-label classification.
+"""
+
 from .brknn import BRkNNaClassifier, BRkNNbClassifier
 from .mlknn import MLkNN
+
+__all__ = ["BRkNNaClassifier", 
+           "BRkNNbClassifier", 
+           "MLkNN"]
