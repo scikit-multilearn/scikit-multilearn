8913a1ca25b51fc52b08187ab67a1e8763015d07
Piotr Szymański <niedakh@gmail.com>
2016-06-15 18:28:09 +0200

    handle ndarray to matrix conversion

--- skmultilearn/utils.py
+++ skmultilearn/utils.py
@@ -1,7 +1,20 @@
+import numpy as np
 import scipy.sparse as sp
 
+SPARSE_FORMAT_TO_CONSTRUCTOR = {
+    "bsr": sp.bsr_matrix,
+    "coo": sp.coo_matrix,
+    "csc": sp.csc_matrix,
+    "csr": sp.csr_matrix,
+    "dia": sp.dia_matrix,
+    "dok": sp.dok_matrix,
+    "lil": sp.lil_matrix
+}
 
 def get_matrix_in_format(original_matrix, matrix_format):
+    if isinstance(original_matrix, np.ndarray):
+        return SPARSE_FORMAT_TO_CONSTRUCTOR[matrix_format](original_matrix)
+
     if original_matrix.getformat() == matrix_format:
         return original_matrix
 
@@ -9,16 +22,6 @@ def get_matrix_in_format(original_matrix, matrix_format):
 
 
 def matrix_creation_function_for_format(sparse_format):
-    SPARSE_FORMAT_TO_CONSTRUCTOR = {
-        "bsr": sp.bsr_matrix,
-        "coo": sp.coo_matrix,
-        "csc": sp.csc_matrix,
-        "csr": sp.csr_matrix,
-        "dia": sp.dia_matrix,
-        "dok": sp.dok_matrix,
-        "lil": sp.lil_matrix
-    }
-
     if sparse_format not in SPARSE_FORMAT_TO_CONSTRUCTOR:
         return None
 
