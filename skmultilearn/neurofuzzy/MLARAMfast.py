70d49a9c604fbb5c9b7f1f56764d18567d2fa707
fbenites <iskywalker@gmx.de>
2016-06-24 21:56:14 +0200

    changed this to be more sklearn conform

--- skmultilearn/neurofuzzy/MLARAMfast.py
+++ skmultilearn/neurofuzzy/MLARAMfast.py
@@ -15,10 +15,22 @@ class Neuron:
         self.label=label
 
 class MLARAM(MLClassifierBase):
-    """Multi-label ARAM   classifier. See http://dx.doi.org/10.1109/ICDMW.2015.14"""
+    """Multi-label ARAM   classifier. See http://dx.doi.org/10.1109/ICDMW.2015.14
+    
+ 
+    Parameters
+    ----------
+
+    vigilance = vigilance parameter for adaptiv resonance theory networks, controls how large a hyperbox can be, 1 it is small (no compression), 0 should assume all range. Normally set between 0.8 and 0.999, it is dataset dependent. It is responsible for the creation of the prototypes, therefore training of the network.    
+    threshold = controls how many prototypes participate by the prediction, can be changed at the testing phase.
+    tneurons =  if the network should inherited neurons (prototypes) from another network
+    tdebug = set debug modus 
+    
+    require_dense : boolean
+        Whether the base classifier requires input as dense arrays, False by default"""
     BRIEFNAME = "ML-ARAM"
-    def __init__(self,classifier = None, vigilance=0.9,threshold=0.02, printouttst=0,tneurons=None, tdebug=0, require_dense = True):
-        super(MLARAM, self).__init__(classifier, require_dense)
+    def __init__(self, vigilance=0.9,threshold=0.02, tneurons=None, tdebug=0, require_dense = True):
+        super(MLARAM, self).__init__(None, require_dense)
         
         if tneurons!=None:
             self.neurons=tneurons
@@ -27,10 +39,11 @@ class MLARAM(MLClassifierBase):
         self.labels=[]
         self.vigilance=vigilance
         self.threshold=threshold
-	self.printouttst=printouttst
+	
         self.allneu=""
         self.debug=tdebug
         self.online=1
+        self.alpha=0.0000000000001
         self.copyable_attrs = ["classifier", "require_dense", "vigilance","threshold","online"]
         
     def reset(self):
@@ -40,9 +53,18 @@ class MLARAM(MLClassifierBase):
     def fit(self,X,y):
         
         labdict = {}
+        if len(X[0].shape)==1:
+            ismatrix=0
+        else:
+            ismatrix=1
+        xma=X.max()
+        xmi=X.min()
+        if xma<0 or xma>1 or xmi<0 or xmi>1:
+            X=numpy.multiply(X-xmi,1/(xma-xmi))
+            
         if len(self.neurons) == 0:
             ones = scipy.ones(X[0].shape);
-            self.neurons.append(Neuron(numpy.concatenate((X[0], ones - X[0]), 1),y[0]))
+            self.neurons.append(Neuron(numpy.concatenate((X[0], ones - X[0]), ismatrix),y[0]))
             startc = 1
             labdict[y[0].nonzero()[0].tostring()] = [0]
         else:
@@ -52,14 +74,14 @@ class MLARAM(MLClassifierBase):
         time1=time.time()
         ones = scipy.ones(X[0].shape);
         for i1,f1 in enumerate(X[startc: ], startc):
-            #print i1,X.shape[0],len(self.neurons), newlabel,len(labdict) 
+
             if i1%1000==0:
                 print i1,X.shape[0],len(self.neurons), newlabel, "time ",time.time()-time1
                 time1=time.time()
             found=0
             if scipy.sparse.issparse(f1):
                 f1=f1.todense()
-            fc = numpy.concatenate((f1, ones - f1), 1)
+            fc = numpy.concatenate((f1, ones - f1), ismatrix)
                 
             activationn = [0] * len(self.neurons)
             activationi = [0] * len(self.neurons)
@@ -70,12 +92,6 @@ class MLARAM(MLClassifierBase):
                     minnfs = umath.minimum(self.neurons[i2].vc, fc).sum()
                     activationi[i2] =minnfs/fcs
                     activationn[i2] =minnfs/self.neurons[i2].vc.sum()
-#            for i2, n1 in enumerate(self.neurons):                
-#                 if (y[i1] - n1.label).sum() == 0:
-#                     activation[i2] =umath.minimum(n1.vc, fc).sum()/fc.sum()
-
-
-                     #activation[i2] = numpy.concatenate((umath.minimum(n1.down,f1),umath.minimum(ones-n1.up,ones-f1)),1).sum()/float(numpy.concatenate((f1,ones-f1),1).sum())
             
 
             if numpy.max(activationn) == 0:
@@ -87,7 +103,6 @@ class MLARAM(MLClassifierBase):
                 continue
             inds = numpy.argsort(activationn)
             
-            # if activationi[inds[ - 1]] < self.vigilance:
             indc = numpy.where(numpy.array(activationi)[inds[::-1]]>self.vigilance)[0]
             if indc.shape[0] == 0: 
                 self.neurons.append(Neuron(fc,y[i1]))
@@ -98,10 +113,9 @@ class MLARAM(MLClassifierBase):
 
             winner =inds[::- 1][indc[0]]
             self.neurons[winner].vc= umath.minimum(self.neurons[winner].vc,fc)
-            #print i1,winner, len(self.neurons),self.neurons[winner].vc.sum()
+            
 
-            #if winner==407:
-            #    print i1
+            
             labadd = numpy.zeros(y[0].shape,dtype=y[0].dtype)
             labadd[y[i1].nonzero()] = 1
             self.neurons[winner].label +=   labadd
@@ -110,95 +124,9 @@ class MLARAM(MLClassifierBase):
     
     #@profile
     def predict(self,X):
-        ft = 0
-        tt=0
-        fn = 0
-        tp=0
-        fp=0
-        mrec=0
-        mpre=0
-        macf1=0
-        result = []
-        if len(X) == 0: 
-            return
-        ones = scipy.ones(X[0].shape);
-        n1s = [0] *  len(self.neurons)
-        allranks = []
-        neuronsactivated=[]
-        #for i in range(0, len(self.neurons)):
-        #    n1s[i] = self.neurons[i].vc.sum()
-
-        #if self.debug==1:
-        #    import time
-        #    start = time.time()
-        #allneu=numpy.concatenate(([n1.vc for n1 in self.neurons]))
-        allneu=numpy.vstack([n1.vc for n1 in self.neurons])
-        allneusum=allneu.sum(1)
-        #if self.debug==1:            
-        #    elapsed = (time.time() - start)
-        #    print "for calculation of big neurons matrix ",elapsed
-        import time
-        time1=time.time()
-        for i1,f1 in enumerate(X):
-            if self.printouttst==1:
-                print i1,
-            if (i1%10)+1==10:
-                #print i1
-                print i1,time.time()-time1
-                time1=time.time()
-
-            if scipy.sparse.issparse(f1):
-
-                f1 = f1.todense()
-            fc = numpy.concatenate((f1, ones - f1), 1)
-            # numpy with umath broadcast itself: http://www.scipy.org/EricsBroadcastingDoc
-            #fca=numpy.tile(fc,(len(self.neurons),1))
-            #activity=[0] * len(self.neurons)
-            #for i2,n1 in enumerate(self.neurons):
-            #    activity[i2] =umath.minimum(n1.vc, fc).sum()/n1s[i2]
-                # this does NOT make a copy of a, simply indexes it backward, and should
-            #if self.debug==1:
-            #    start = time.time()
-            activity=(umath.minimum(fc,allneu).sum(1)/allneusum).squeeze().tolist()[0]
-            
-            # be very fast
-            sortedact=numpy.argsort(activity)[::-1]
-            
-            #if self.debug==1:            
-            #    elapsed = (time.time() - start)
-            #    print "for activation and sorting  ",elapsed
-
-            winner=sortedact[0]
-            diff_act=activity[winner]-activity[sortedact[-1]]
-            #print i1,winner
-            
-
-            largest_activ = 1;
-
-            par_t=self.threshold
-            for i in range(1, len(self.neurons)):
-                activ_change = (activity[winner]-activity[sortedact[i]])/activity[winner];
-                if activ_change >par_t*diff_act:
-                    break
-
-                largest_activ +=  1;
-
-            # print self.neurons[winner].label,labels, winner
-
-            rbsum = sum([activity[k] for k in sortedact[0:largest_activ]])
-
-            rank = activity[winner]*self.neurons[winner].label
-            actives =[]
-            activity_actives =[]
-            actives.append(winner)
-            activity_actives.append(activity[winner])
-            for i in range(1,largest_activ):
-                rank+=activity[sortedact[i]]*self.neurons[sortedact[i]].label
-                actives.append(sortedact[i])
-                activity_actives.append(activity[sortedact[i]])
-            rank/= rbsum
-            
-
+        result=[]
+        ranks=self.predict_proba(X)
+        for rank in ranks:
             sortedRankarg = numpy.argsort(-rank)
             diffs=-numpy.diff([rank[k] for k in sortedRankarg])
             
@@ -212,74 +140,55 @@ class MLARAM(MLClassifierBase):
             label[sortedRankarg[0:indcut]]=1
                 
             result.append(label)
-            allranks.append(rank)
-            neuronsactivated.append((actives,activity_actives))
                 
         return numpy.array(numpy.matrix(result))
 
     #@profile
     def predict_proba(self,X):
-        ft = 0
-        tt=0
-        fn = 0
-        tp=0
-        fp=0
-        mrec=0
-        mpre=0
-        macf1=0
         result = []
         if len(X) == 0: 
             return
+        if len(X[0].shape)==1:
+            ismatrix=0
+        else:
+            ismatrix=1
+        xma=X.max()
+        xmi=X.min()
+        if xma<0 or xma>1 or xmi<0 or xmi>1:
+            X=numpy.multiply(X-xmi,1/(xma-xmi))
         ones = scipy.ones(X[0].shape);
         n1s = [0] *  len(self.neurons)
         allranks = []
         neuronsactivated=[]
-        #for i in range(0, len(self.neurons)):
-        #    n1s[i] = self.neurons[i].vc.sum()
 
-        #if self.debug==1:
-        #    import time
-        #    start = time.time()
-        #allneu=numpy.concatenate(([n1.vc for n1 in self.neurons]))
         allneu=numpy.vstack([n1.vc for n1 in self.neurons])
-        allneusum=allneu.sum(1)
-        #if self.debug==1:            
-        #    elapsed = (time.time() - start)
-        #    print "for calculation of big neurons matrix ",elapsed
+        allneusum=allneu.sum(1)+self.alpha
+
+
         import time
         time1=time.time()
         for i1,f1 in enumerate(X):
-            if self.printouttst==1:
+            if self.debug==1:
                 print i1,
             if (i1%10)+1==10:
-                #print i1
                 print i1,time.time()-time1
                 time1=time.time()
 
             if scipy.sparse.issparse(f1):
 
                 f1 = f1.todense()
-            fc = numpy.concatenate((f1, ones - f1), 1)
-            # numpy with umath broadcast itself: http://www.scipy.org/EricsBroadcastingDoc
-            #fca=numpy.tile(fc,(len(self.neurons),1))
-            #activity=[0] * len(self.neurons)
-            #for i2,n1 in enumerate(self.neurons):
-            #    activity[i2] =umath.minimum(n1.vc, fc).sum()/n1s[i2]
-                # this does NOT make a copy of a, simply indexes it backward, and should
-            #if self.debug==1:
-            #    start = time.time()
-            activity=(umath.minimum(fc,allneu).sum(1)/allneusum).squeeze().tolist()[0]
+            fc = numpy.concatenate((f1, ones - f1), ismatrix)
+            activity=(umath.minimum(fc,allneu).sum(1)/allneusum).squeeze().tolist()
+            if ismatrix==1:
+                activity=activity[0]
             
             # be very fast
             sortedact=numpy.argsort(activity)[::-1]
             
-            #if self.debug==1:            
-            #    elapsed = (time.time() - start)
-            #    print "for activation and sorting  ",elapsed
 
             winner=sortedact[0]
             diff_act=activity[winner]-activity[sortedact[-1]]
-            #print i1,winner
+
             
 
             largest_activ = 1;
@@ -292,7 +201,7 @@ class MLARAM(MLClassifierBase):
 
                 largest_activ +=  1;
 
-            # print self.neurons[winner].label,labels, winner
+
 
             rbsum = sum([activity[k] for k in sortedact[0:largest_activ]])
 
