ebb2d4051652bc9c07f979d932a37d624f15848e
Piotr Szymanski <niedakh@gmail.com>
2017-02-08 23:30:38 +0100

    fix imports in ensembles

---
+++ skmultilearn/ensemble/__init__.py
@@ -0,0 +1,4 @@
+from rakeld import RakelD
+from rakelo import RakelO
+from fixed import FixedLabelPartitionClassifier, LabelSpacePartitioningClassifier
+from partition import LabelSpacePartitioningClassifier
