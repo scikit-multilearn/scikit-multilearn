8dc4770e74a13c38501125efe014b62966bc05bb
Piotr Szymanski <niedakh@gmail.com>
2017-02-10 17:17:16 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scikit-multilearn',
-    version='0.0.3.89',
+    version='0.0.4',
     packages=find_packages(exclude=['docs', 'tests', '*.tests']),
     author=u'Piotr Szymański',
     author_email='niedakh@gmail.com',
