b6d91c962c626f8b93a0cd5535967012429c08db
Piotr Szymanski <niedakh@gmail.com>
2017-02-25 03:31:49 +0100

    import the matrix clusterer

--- skmultilearn/cluster/__init__.py
+++ skmultilearn/cluster/__init__.py
@@ -1,13 +1,16 @@
 """
 The :mod:`skmultilearn.cluster` module gathers label space clustering methods.
+
 """
 
 from .base import LabelSpaceClustererBase
 from .base import LabelCooccurenceClustererBase
 from .graphtool import GraphToolCooccurenceClusterer
 from .igraph import IGraphLabelCooccurenceClusterer
+from .matrix import MatrixLabelSpaceClusterer
 
 __all__ = ['LabelSpaceClustererBase',
            'LabelCooccurenceClustererBase',
            'GraphToolCooccurenceClusterer',
-           'IGraphLabelCooccurenceClusterer']
+           'IGraphLabelCooccurenceClusterer',
+           'MatrixLabelSpaceClusterer']
