c9191da7ae8a2a67753c8b66a43dcfd69ddd6260
Piotr Szymanski <niedakh@gmail.com>
2017-02-25 01:41:27 +0100

    fix clonability of mlaram, autopep

--- skmultilearn/neurofuzzy/tests/test_mlaramfast.py
+++ skmultilearn/neurofuzzy/tests/test_mlaramfast.py
@@ -5,24 +5,16 @@ from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
 
 
 class MLARAMTest(ClassifierBaseTest):
-    #def test_if_sparse_classification_works_on_non_dense_base_classifier(self):
-    #    classifier = (classifier = SVC(), require_dense = [False, True])
-    #    self.assertClassifierWorksWithSparsity(classifier, 'sparse')
-
-    #def test_if_dense_classification_works_on_non_dense_base_classifier(self):
-    #    classifier = MLARAM(require_dense =  True)
-
-    #    self.assertClassifierWorksWithSparsity(classifier, 'dense')
-
-    #def test_if_sparse_classification_works_on_dense_base_classifier(self):
-    #    classifier = ClassifierChain(classifier = GaussianNB(), require_dense = [True, True])
-
-    #    self.assertClassifierWorksWithSparsity(classifier, 'sparse')
 
     def test_if_dense_classification_works_on_dense_base_classifier(self):
         classifier = MLARAM()
 
         self.assertClassifierWorksWithSparsity(classifier, 'dense')
+
+    def test_if_works_with_cross_validation(self):
+        classifier = MLARAM()
+
+        self.assertClassifierWorksWithCV(classifier)
 
 if __name__ == '__main__':
     unittest.main()
