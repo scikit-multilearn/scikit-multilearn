d1bd6f67472ddcdf7958fb5b95c5aab0ae609ec7
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-09-29 08:28:51 +0900

    Check tests\neurofuzzy (#73)
    Ensure that tests for neurofuzzy is working.

--- tests/neurofuzzy/test_mlaramfast.py
+++ tests/neurofuzzy/test_mlaramfast.py
@@ -1,19 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Test cases for skmultilearn.neurofuzzy.MLARAM"""
+
+# Import modules
 import unittest
 
+# Import from package
 from skmultilearn.neurofuzzy import MLARAM
-from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
+
+# Import from test suite
+from tests.testsuite.classifier_base import ClassifierBaseTest
 
 
 class MLARAMTest(ClassifierBaseTest):
+    """Test cases for MLARAM class"""
 
     def test_if_dense_classification_works_on_dense_base_classifier(self):
         classifier = MLARAM()
-
         self.assertClassifierWorksWithSparsity(classifier, 'dense')
 
     def test_if_works_with_cross_validation(self):
         classifier = MLARAM()
-
         self.assertClassifierWorksWithCV(classifier)
 
 if __name__ == '__main__':
