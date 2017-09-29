280e83b8c2bbb3959229a1c2e2224577ab851ed5
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2017-09-29 09:47:16 +0900

    Check tests/ext (#73)
    This commit checks if the tests/ext is passing. For now,
    I added a skipIf decorator that skips all checks if the
    MEKA_CLASSPATH is undefined (or a NoneType)

--- tests/ext/test_meka.py
+++ tests/ext/test_meka.py
@@ -1,24 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""Test cases for skmultilearn.ext module"""
+
+# Import modules
+import os
 import unittest
+import argparse
 
+# Import from package
 from skmultilearn.ext import Meka
-from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
 
+# Import from test suite
+from tests.testsuite.classifier_base import ClassifierBaseTest
 
+# Check if MEKA_CLASPATH exists in the environment variables
+check_env = lambda : os.environ.get('MEKA_CLASSPATH')
+
+@unittest.skipIf(check_env()==None, 'No MEKA_CLASSPATH found. Skipping MEKA extension test')
 class MekaTest(ClassifierBaseTest):
+    """Test cases for Meka"""
 
-    def classifier(self):
+    def setUp(self):
+        """Set-up test fixtures"""
         meka_classifier = "meka.classifiers.multilabel.BR"
         weka_classifier = "weka.classifiers.rules.ZeroR"
-        return Meka(meka_classifier=meka_classifier, weka_classifier=weka_classifier)
+        self.classifier = Meka(meka_classifier=meka_classifier, 
+                            weka_classifier=weka_classifier)
 
     def test_if_meka_classification_works_on_sparse_input(self):
-        self.assertClassifierWorksWithSparsity(self.classifier(), 'sparse')
+        self.assertClassifierWorksWithSparsity(self.classifier, 'sparse')
 
     def test_if_meka_classification_works_on_dense_input(self):
-        self.assertClassifierWorksWithSparsity(self.classifier(), 'dense')
+        self.assertClassifierWorksWithSparsity(self.classifier, 'dense')
 
     def test_if_works_with_cross_validation(self):
-        self.assertClassifierWorksWithCV(self.classifier())
+        self.assertClassifierWorksWithCV(self.classifier)
 
 if __name__ == '__main__':
     unittest.main()
