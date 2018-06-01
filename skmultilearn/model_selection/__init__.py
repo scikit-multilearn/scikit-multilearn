d1c0e9979bf9e341e487b3fa8f38db6489810773
Piotr Szymanski <niedakh@gmail.com>
2018-06-01 16:39:14 +0200

    fix imports

--- skmultilearn/model_selection/__init__.py
+++ skmultilearn/model_selection/__init__.py
@@ -1,1 +1,8 @@
 from .iterative_stratification import IterativeStratification
+from .measures import (
+    example_distribution,
+    label_combination_distribution,
+    percentage_of_label_combinations_without_evidence_per_fold,
+    folds_without_evidence_for_at_least_one_label_combination,
+    folds_label_combination_pairs_without_evidence
+)
--- skmultilearn/model_selection/tests/test_measures.py
+++ skmultilearn/model_selection/tests/test_measures.py
@@ -1,6 +1,6 @@
 import unittest
 from .fixtures import FIXTURE_Y, FOLDS_FIXTURES
-from skmultilearn.measures.imbalance import (
+from skmultilearn.model_selection import (
     example_distribution, label_combination_distribution, percentage_of_label_combinations_without_evidence_per_fold,
     folds_without_evidence_for_at_least_one_label_combination, folds_label_combination_pairs_without_evidence
 )
