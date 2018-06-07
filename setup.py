922f11ab909fc6a34b1dd2abb1038aad38018fa4
Piotr Szymanski <niedakh@gmail.com>
2018-06-07 23:53:01 +0200

    tag release candidate, add readme

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-#import sphinx_pypi_upload
+# import sphinx_pypi_upload
+import codecs
+
+with codecs.open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name='scikit-multilearn',
-    version='0.0.5',
+    version='0.1.0rc1',
     packages=find_packages(exclude=['docs', 'tests', '*.tests']),
     author=u'Piotr Szymański',
-    author_email='niedakh@gmail.com',
-    license='BSD',
-#    long_description=open('README.md').read(),
-    url='http://scikit-multilearn.github.io/',
-    description= 'A set of python modules for multi-label classification',
+    author_email=u'niedakh@gmail.com',
+    license=u'BSD',
+    long_description=readme,
+    url=u'http://scikit-multilearn.github.io/',
+    description=u'A set of python modules for multi-label classification',
     classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Education',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Topic :: Scientific/Engineering',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'Topic :: Scientific/Engineering :: Bio-Informatics',
-          ],
+        u'Development Status :: 5 - Production/Stable',
+        u'Environment :: Console',
+        u'Environment :: Web Environment',
+        u'Intended Audience :: Developers',
+        u'Intended Audience :: Education',
+        u'Intended Audience :: Science/Research',
+        u'License :: OSI Approved :: BSD License',
+        u'Operating System :: MacOS :: MacOS X',
+        u'Operating System :: Microsoft :: Windows',
+        u'Operating System :: POSIX',
+        u'Programming Language :: Python',
+        u'Topic :: Scientific/Engineering',
+        u'Topic :: Scientific/Engineering :: Information Analysis',
+        u'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
 )
