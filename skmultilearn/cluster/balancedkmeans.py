fb30e9d16da9ef10ad4b6a20cb6011b8c60a25c1
Jopepato <jopepato@localhost.localdomain>
2018-06-20 13:18:32 +0200

    Added class of balancedKMeans and functions for it to work

--- skmultilearn/cluster/balancedkmeans.py
+++ skmultilearn/cluster/balancedkmeans.py
@@ -4,15 +4,42 @@ import numpy as np
 import random
 
 from .base import LabelSpaceClustererBase
+from .helpers import _euclidean_distance, _recalculateCenters, _countNumberOfAparitions
 
 class balancedKMeansClusterer(LabelSpaceClustererBase):
-    "Balanced clustering algorithm which is used by HOMER"
+    """Cluster the label space regarding the algorithm of balancedkMeans, used by HOMER"""
+
     def __init__(self, k = None, it = None):
+        """Initializes the clusterer
+
+        Attributes
+        ----------
+        k: int
+                Number of partitions to be made to the label-space
+        it: int
+                Number of iterations for the algorithm to find the best neighbours
+        """
         super(balancedKMeansClusterer, self).__init__()
         self.k = k
         self.it = it
 
     def fit_predict(self, X, y):
+        """Performs clustering on y and returns list of label lists
+
+        Builds a label list taking care of the distance between labels
+
+        Parameters
+        ----------
+        X : currently unused, left for scikit compatibility
+        y : scipy.sparse
+            label space of shape :code:`(n_samples, n_labels)`
+
+        Returns
+        -------
+        array of arrays
+            numpy array of arrays of label indexes, where each sub-array
+            represents labels that are in a separate community
+        """
         number_of_labels = y.shape[1]
         #Assign a label to a cluster no. label ordinal %  number of labeSls
         #We have to do the balance k-means and then use it for HOMER with the label powerset
@@ -32,13 +59,13 @@ class balancedKMeansClusterer(LabelSpaceClustererBase):
                 distances = []
                 for i in range(0, self.k):
                     #Store the distances
-                    distances.append(euclidean_distance(v, Centers[i]))
+                    distances.append(_euclidean_distance(v, Centers[i]))
                 finished = False
                 while not finished:
                     minIndex = np.argmin(distances)
                     balancedCluster.append(minIndex)
                     #Now we have the cluster we want to add this label to
-                    numberOfAparitions = countNumberOfAparitions(balancedCluster, minIndex)
+                    numberOfAparitions = _countNumberOfAparitions(balancedCluster, minIndex)
                     if float(numberOfAparitions) > (float(float(number_of_labels)/float(self.k))+1):
                         distances[minIndex] = float("inf")
                         balancedCluster.pop()
@@ -46,7 +73,7 @@ class balancedKMeansClusterer(LabelSpaceClustererBase):
                         finished = True
 
                 
-            Centers = recalculateCenters(np.asarray(y), balancedCluster, self.k)
+            Centers = _recalculateCenters(np.asarray(y), balancedCluster, self.k)
             self.it = self.it -1
 
         #Returns a list of list with the clusterers
--- skmultilearn/cluster/helpers.py
+++ skmultilearn/cluster/helpers.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+
 def _membership_to_list_of_communities(membership_vector, size):
     """Convert membership vector to list of lists of vertices in each community
 
@@ -41,3 +44,65 @@ def _overlapping_membership_to_list_of_communities(membership_vector, size):
         for community_id in community_ids:
             list_of_members[community_id].append(vertex_id)
     return list_of_members
+
+def _euclidean_distance(array1, array2):
+    """Returns the euclidean distance of two arrays
+
+    Parameters
+    ----------
+    array1 : array of numbers
+        
+    array2 : array of numbers
+
+    Returns
+    -------
+    distance : float
+        float with the euclidean distance, False if not possible
+    """
+    #Ensure that both arrays hava the same length
+    if len(array1) != len(array2):
+        return False
+    else:
+        distance = 0.0
+        for i in range(0, len(array1)):
+            distance = distance + pow(array1[i] - array2[i], 2)
+        distance = math.sqrt(distance)
+        return distance
+
+def _recalculateCenters(y, balancedCluster, k):
+
+    Centers = []
+    kAux = 0
+    while kAux < k:
+        vectorAux = np.zeros(len(y))
+        for i in range(0, len(balancedCluster)):
+
+            if int(kAux) == int(balancedCluster[i]):
+                #We have to fill the vector
+                for j in range(0, len(y)):
+                    vectorAux[j] += y[j,i]
+
+        vectorAux /= k
+        Centers.append(vectorAux)
+        kAux += 1
+    return Centers
+
+def _countNumberOfAparitions(array, number):
+    """Number of aparitions of a number in an array
+
+    Parameters
+    ----------
+    array : array of numbers
+        
+    number : number to search for
+
+    Returns
+    -------
+    aparaitions : int
+        Number of aparitions of the number in the given array
+    """
+    aparitions = 0
+    for i in range(0, len(array)):
+        if array[i] == number:
+            aparitions += 1
+    return aparitions
