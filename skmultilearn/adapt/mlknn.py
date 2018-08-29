0e731a2c6d16006256102f66eef052eeb93fa757
Piotr Szymanski <niedakh@gmail.com>
2018-08-29 18:31:02 +0200

    updated docs, hide private methods

--- skmultilearn/adapt/mlknn.py
+++ skmultilearn/adapt/mlknn.py
@@ -8,21 +8,109 @@ import scipy.sparse as sparse
 
 
 class MLkNN(MLClassifierBase):
-    """kNN classification method adapted for multi-label classification"""
-    BRIEFNAME = "MLkNN"
+    """kNN classification method adapted for multi-label classification
+
+    MLkNN builds uses k-NearestNeighbors find nearest examples to a test class and uses Bayesian inference
+    to select assigned labels.
+
+    Parameters
+    ----------
+    k : int
+        number of neighbours of each input instance to take into account
+    s: float (default is 1.0)
+        the smoothing parameter
+    ignore_first_neighbours : int (default is 0)
+            ability to ignore first N neighbours, useful for comparing
+            with other classification software.
+
+    Attributes
+    ----------
+    knn_ : an instance of sklearn.NearestNeighbors
+        the nearest neighbors single-label classifier used underneath
+
+
+
+    .. note:: If you don't know what :code:`ignore_first_neighbours`
+              does, the default is safe. Please see this `issue`_.
+
+
+    .. _issue: https://github.com/scikit-multilearn/scikit-multilearn/issues/22
+
+
+    References
+    ----------
+
+    If you use this classifier please cite the original paper introducing the method:
+
+    .. code :: bibtex
+
+        @article{zhang2007ml,
+          title={ML-KNN: A lazy learning approach to multi-label learning},
+          author={Zhang, Min-Ling and Zhou, Zhi-Hua},
+          journal={Pattern recognition},
+          volume={40},
+          number={7},
+          pages={2038--2048},
+          year={2007},
+          publisher={Elsevier}
+        }
+
+    Examples
+    --------
+
+    Here's a very simple example of using MLkNN with a fixed number of neighbors:
+
+    .. code :: python
+
+        from skmultilearn.adapt import MLkNN
+
+        classifier = MLkNN(k=3)
+
+        # train
+        classifier.fit(X_train, y_train)
+
+        # predict
+        predictions = classifier.predict(X_test)
+
+
+    You can also use :class:`~sklearn.model_selection.GridSearchCV` to find an optimal set of parameters:
+
+    .. code :: python
+
+        from skmultilearn.adapt import MLkNN
+        from sklearn.model_selection import GridSearchCV
+
+        parameters = {'k': range(1,3), 's': [0.5, 0.7, 1.0]}
+        score = 'f1-macro
+
+        clf = GridSearchCV(MLkNN(), parameters, scoring=score)
+        clf.fit(X, y)
+
+        print clf.best_params_, clf.best_score_
+
+        # output
+        ({'k': 1, 's': 0.5}, 0.78988303374297597)
+
+    """
 
     def __init__(self, k=10, s=1.0, ignore_first_neighbours=0):
         """Initializes the classifier
 
-        Attributes
+        Parameters
         ----------
-        k : int (default is 10)
+        k : int
             number of neighbours of each input instance to take into account
         s: float (default is 1.0)
             the smoothing parameter
         ignore_first_neighbours : int (default is 0)
                 ability to ignore first N neighbours, useful for comparing
-                with other classification software. 
+                with other classification software.
+
+
+        Attributes
+        ----------
+        knn_ : an instance of sklearn.NearestNeighbors
+            the nearest neighbors single-label classifier used underneath
 
         .. note:: If you don't know what :code:`ignore_first_neighbours`
                   does, the default is safe. Please see this `issue`_.
@@ -35,9 +123,9 @@ class MLkNN(MLClassifierBase):
         self.ignore_first_neighbours = ignore_first_neighbours
         self.copyable_attrs = ['k', 's', 'ignore_first_neighbours']
 
-    def compute_prior(self, y):
+    def _compute_prior(self, y):
         """Helper function to compute for the prior probabilities
-        
+
         Parameters
         ----------
         y : numpy.ndarray or scipy.sparse
@@ -50,14 +138,14 @@ class MLkNN(MLClassifierBase):
         numpy.ndarray
             the prior probability given false
         """
-        prior_prob_true = np.array((self.s + y.sum(axis=0)) / (self.s * 2 + self.num_instances))[0]
+        prior_prob_true = np.array((self.s + y.sum(axis=0)) / (self.s * 2 + self._num_instances))[0]
         prior_prob_false = 1 - prior_prob_true
 
         return (prior_prob_true, prior_prob_false)
 
-    def compute_cond(self, X, y):
+    def _compute_cond(self, X, y):
         """Helper function to compute for the posterior probabilities
-        
+
         Parameters
         ----------
         X : numpy.ndarray or scipy.sparse
@@ -74,18 +162,18 @@ class MLkNN(MLClassifierBase):
             the posterior probability given false
         """
 
-        self.knn = NearestNeighbors(self.k).fit(X)
-        c = sparse.lil_matrix((self.num_labels, self.k + 1), dtype='i8')
-        cn = sparse.lil_matrix((self.num_labels, self.k + 1), dtype='i8')
+        self.knn_ = NearestNeighbors(self.k).fit(X)
+        c = sparse.lil_matrix((self._num_labels, self.k + 1), dtype='i8')
+        cn = sparse.lil_matrix((self._num_labels, self.k + 1), dtype='i8')
 
         label_info = get_matrix_in_format(y, 'dok')
 
         neighbors = [a[self.ignore_first_neighbours:] for a in
-                     self.knn.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
+                     self.knn_.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
 
-        for instance in range(self.num_instances):
+        for instance in range(self._num_instances):
             deltas = label_info[neighbors[instance], :].sum(axis=0)
-            for label in range(self.num_labels):
+            for label in range(self._num_labels):
                 if label_info[instance, label] == 1:
                     c[label, deltas[0, label]] += 1
                 else:
@@ -94,14 +182,14 @@ class MLkNN(MLClassifierBase):
         c_sum = c.sum(axis=1)
         cn_sum = cn.sum(axis=1)
 
-        cond_prob_true = sparse.lil_matrix((self.num_labels, self.k + 1), dtype='float')
-        cond_prob_false = sparse.lil_matrix((self.num_labels, self.k + 1), dtype='float')
-        for label in range(self.num_labels):
+        cond_prob_true = sparse.lil_matrix((self._num_labels, self.k + 1), dtype='float')
+        cond_prob_false = sparse.lil_matrix((self._num_labels, self.k + 1), dtype='float')
+        for label in range(self._num_labels):
             for neighbor in range(self.k + 1):
                 cond_prob_true[label, neighbor] = (self.s + c[label, neighbor]) / (
-                self.s * (self.k + 1) + c_sum[label, 0])
+                        self.s * (self.k + 1) + c_sum[label, 0])
                 cond_prob_false[label, neighbor] = (self.s + cn[label, neighbor]) / (
-                self.s * (self.k + 1) + cn_sum[label, 0])
+                        self.s * (self.k + 1) + cn_sum[label, 0])
         return cond_prob_true, cond_prob_false
 
     def fit(self, X, y):
@@ -117,17 +205,17 @@ class MLkNN(MLClassifierBase):
 
         Returns
         -------
-        skmultilearn.adapt.mlknn.MLkNN
+        self
             fitted instance of self
         """
 
-        self.train_labels = get_matrix_in_format(y, 'lil')
-        self.num_instances = self.train_labels.shape[0]
-        self.num_labels = self.train_labels.shape[1]
+        self._label_cache = get_matrix_in_format(y, 'lil')
+        self._num_instances = self._label_cache.shape[0]
+        self._num_labels = self._label_cache.shape[1]
         # Computing the prior probabilities
-        self.prior_prob_true, self.prior_prob_false = self.compute_prior(self.train_labels)
+        self._prior_prob_true, self._prior_prob_false = self._compute_prior(self._label_cache)
         # Computing the posterior probabilities
-        self.cond_prob_true, self.cond_prob_false = self.compute_cond(X, self.train_labels)
+        self._cond_prob_true, self._cond_prob_false = self._compute_cond(X, self._label_cache)
         return self
 
     def predict(self, X):
@@ -140,20 +228,20 @@ class MLkNN(MLClassifierBase):
 
         Returns
         -------
-        scipy.sparse of int
+        scipy.sparse matrix of int
             binary indicator matrix with label assignments with shape
             :code:`(n_samples, n_labels)`
         """
 
-        result = sparse.lil_matrix((X.shape[0], self.num_labels), dtype='i8')
+        result = sparse.lil_matrix((X.shape[0], self._num_labels), dtype='i8')
         neighbors = [a[self.ignore_first_neighbours:] for a in
-                     self.knn.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
+                     self.knn_.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
         for instance in range(X.shape[0]):
-            deltas = self.train_labels[neighbors[instance],].sum(axis=0)
+            deltas = self._label_cache[neighbors[instance],].sum(axis=0)
 
-            for label in range(self.num_labels):
-                p_true = self.prior_prob_true[label] * self.cond_prob_true[label, deltas[0, label]]
-                p_false = self.prior_prob_false[label] * self.cond_prob_false[label, deltas[0, label]]
+            for label in range(self._num_labels):
+                p_true = self._prior_prob_true[label] * self._cond_prob_true[label, deltas[0, label]]
+                p_false = self._prior_prob_false[label] * self._cond_prob_false[label, deltas[0, label]]
                 result[instance, label] = int(p_true >= p_false)
 
         return result
@@ -168,20 +256,18 @@ class MLkNN(MLClassifierBase):
 
         Returns
         -------
-        scipy.sparse of int
+        scipy.sparse matrix of int
             binary indicator matrix with label assignment probabilities
             with shape :code:`(n_samples, n_labels)`
         """
-        result = sparse.lil_matrix((X.shape[0], self.num_labels), dtype='float')
+        result = sparse.lil_matrix((X.shape[0], self._num_labels), dtype='float')
         neighbors = [a[self.ignore_first_neighbours:] for a in
-                     self.knn.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
+                     self.knn_.kneighbors(X, self.k + self.ignore_first_neighbours, return_distance=False)]
         for instance in range(X.shape[0]):
-            deltas = self.train_labels[neighbors[instance],].sum(axis=0)
+            deltas = self._label_cache[neighbors[instance],].sum(axis=0)
 
-            for label in range(self.num_labels):
-                p_true = self.prior_prob_true[label] * self.cond_prob_true[label, deltas[0, label]]
-                p_false = self.prior_prob_false[label] * self.cond_prob_false[label, deltas[0, label]]
+            for label in range(self._num_labels):
+                p_true = self._prior_prob_true[label] * self._cond_prob_true[label, deltas[0, label]]
                 result[instance, label] = p_true
 
         return result
-
