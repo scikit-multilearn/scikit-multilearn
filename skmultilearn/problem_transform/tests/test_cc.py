608b43fb6b23fbb1db40a824a385d4cf555481f9
Piotr Szymanski <niedakh@gmail.com>
2018-09-02 15:35:18 +0200

    fix classifier chain test

--- skmultilearn/problem_transform/tests/test_cc.py
+++ skmultilearn/problem_transform/tests/test_cc.py
@@ -1,7 +1,6 @@
 import unittest
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from ...tests.example import EXAMPLE_X, EXAMPLE_y
 
 from skmultilearn.problem_transform import ClassifierChain
 from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
@@ -47,18 +46,21 @@ class CCTest(ClassifierBaseTest):
         classifier = ClassifierChain(
             classifier=GaussianNB(), require_dense=[True, True], order=None
         )
+        X, y = self.get_multilabel_data_for_tests(sparsity_indicator='sparse')[0]
 
-        classifier.fit(EXAMPLE_X, EXAMPLE_y)
+        classifier.fit(X,y)
 
-        self.assertEqual(classifier._order(), list(range(EXAMPLE_y.shape[1])))
+        self.assertEqual(classifier._order(), list(range(y.shape[1])))
 
-    def test_if_order_is_set(self):
-        reversed_chain = list(reversed(range(EXAMPLE_y.shape[1])))
+    def test_if_order_is_set_when_explicitly_given(self):
+        X, y = self.get_multilabel_data_for_tests(sparsity_indicator='sparse')[0]
+
+        reversed_chain = list(reversed(range(y.shape[1])))
         classifier = ClassifierChain(
             classifier=GaussianNB(), require_dense=[True, True], order=reversed_chain
         )
 
-        classifier.fit(EXAMPLE_X, EXAMPLE_y)
+        classifier.fit(X, y)
 
         self.assertEqual(classifier._order(), reversed_chain)
 
