727cdd01eaf5f953aa5511890c16dc317c44745a
Piotr Szymanski <niedakh@gmail.com>
2018-12-07 12:13:30 +0100

    support for keras compatible classifiers

---
+++ skmultilearn/ext/keras.py
@@ -0,0 +1,31 @@
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.base import BaseEstimator
+from copy import copy
+
+class Keras(BaseEstimator):
+    def __init__(self, build_function, multi_class=False, keras_params = None):
+        if not callable(build_function):
+            raise ValueError('Model construction function must be callable.')
+
+        self.multi_class = multi_class
+        self.build_function = build_function
+        if keras_params is None:
+            keras_params = {}
+
+        self.keras_params = keras_params
+
+    def fit(self, X, y):
+        if self.multi_class:
+            self.n_classes_ = len(set(y))
+        else:
+            self.n_classes_ = 1
+
+        build_callable = lambda: self.build_function(X.shape[1], self.n_classes_)
+        keras_params=copy(self.keras_params)
+        keras_params['build_fn']=build_callable
+
+        self.classifier_ = KerasClassifier(**keras_params)
+        self.classifier_.fit(X, y)
+
+    def predict(self, X):
+        return self.classifier_.predict(X)
---
+++ skmultilearn/ext/tests/test_keras.py
@@ -0,0 +1,57 @@
+import unittest
+from keras.models import Sequential
+from keras.layers import Dense
+from skmultilearn.ext.keras import Keras
+
+from skmultilearn.problem_transform import BinaryRelevance, LabelPowerset
+from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
+
+KERAS_PARAMS = dict(epochs=150, batch_size=10, verbose=0)
+
+
+def create_model_multiclass(input_dim, output_dim):
+    # create model
+    model = Sequential()
+    model.add(Dense(8, input_dim=input_dim, activation='relu'))
+    model.add(Dense(output_dim, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def create_model_single_class(input_dim, output_dim):
+    # create model
+    model = Sequential()
+    model.add(Dense(12, input_dim=input_dim, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(output_dim, activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+class KerasTest(ClassifierBaseTest):
+
+    def test_if_sparse_classification_works_on_non_dense_base_classifier(self):
+        classifier = BinaryRelevance(classifier=Keras(create_model_single_class, False, KERAS_PARAMS), require_dense=[True, True])
+
+        self.assertClassifierWorksWithSparsity(classifier, 'sparse')
+
+    def test_if_dense_classification_works_on_non_dense_base_classifier(self):
+        classifier = BinaryRelevance(classifier=Keras(create_model_multiclass, False, KERAS_PARAMS), require_dense=[True, True])
+
+        self.assertClassifierWorksWithSparsity(classifier, 'dense')
+
+    def test_if_sparse_classification_works_on_dense_base_classifier(self):
+        classifier = LabelPowerset(classifier=Keras(create_model_multiclass, True, KERAS_PARAMS), require_dense=[True, True])
+
+        self.assertClassifierWorksWithSparsity(classifier, 'sparse')
+
+    def test_if_dense_classification_works_on_dense_base_classifier(self):
+        classifier = LabelPowerset(classifier=Keras(create_model_single_class, True, KERAS_PARAMS), require_dense=[True, True])
+
+        self.assertClassifierWorksWithSparsity(classifier, 'dense')
+
+
+if __name__ == '__main__':
+    unittest.main()
