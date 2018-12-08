b739a8e239975f53c49141948c1c0e9adc4ef2cd
Piotr Szymanski <niedakh@gmail.com>
2018-12-08 09:34:48 +0100

    add documentation

--- skmultilearn/ext/__init__.py
+++ skmultilearn/ext/__init__.py
@@ -10,11 +10,15 @@ Currently the available classes include:
 | :class:`~skmultilearn.ext.Meka`            | Wrapper for the Multilabel Extension to WEKA -                   |
 |                                            | `MEKA <http://meka.sf.net>`_ library                             |
 +--------------------------------------------+------------------------------------------------------------------+
+| :class:`~skmultilearn.ext.Keras`           | Wrapper for the Python Deep Learning library -                   |
+|                                            | `KERAS <http://https://keras.io/>`_                              |
++--------------------------------------------+------------------------------------------------------------------+
 | :func:`~skmultilearn.ext.download_meka`    | Helper function for installing MEKA                              |
 +--------------------------------------------+------------------------------------------------------------------+
 
 """
 
 from .meka import Meka, download_meka
+from .keras import Keras
 
-__all__ = ["Meka", 'download_meka']
+__all__ = ["Meka", 'download_meka', 'Keras']
