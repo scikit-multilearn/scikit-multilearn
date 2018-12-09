2eb528ef4f8e3a37ecd599fa5d918735ab74ce1d
Piotr Szymanski <niedakh@gmail.com>
2018-12-09 03:59:17 +0100

    fix test, add requirements and fix windows build

--- skmultilearn/ext/tests/test_keras.py
+++ skmultilearn/ext/tests/test_keras.py
@@ -6,7 +6,7 @@ from skmultilearn.ext.keras import Keras
 from skmultilearn.problem_transform import BinaryRelevance, LabelPowerset
 from skmultilearn.tests.classifier_basetest import ClassifierBaseTest
 
-KERAS_PARAMS = dict(epochs=150, batch_size=10, verbose=0)
+KERAS_PARAMS = dict(epochs=1, batch_size=10, verbose=0)
 
 
 def create_model_multiclass(input_dim, output_dim):
@@ -20,7 +20,6 @@ def create_model_multiclass(input_dim, output_dim):
 
 
 def create_model_single_class(input_dim, output_dim):
-    # create model
     model = Sequential()
     model.add(Dense(12, input_dim=input_dim, activation='relu'))
     model.add(Dense(8, activation='relu'))
@@ -28,7 +27,6 @@ def create_model_single_class(input_dim, output_dim):
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-
 
 class KerasTest(ClassifierBaseTest):
 
@@ -38,7 +36,7 @@ class KerasTest(ClassifierBaseTest):
         self.assertClassifierWorksWithSparsity(classifier, 'sparse')
 
     def test_if_dense_classification_works_on_non_dense_base_classifier(self):
-        classifier = BinaryRelevance(classifier=Keras(create_model_multiclass, False, KERAS_PARAMS), require_dense=[True, True])
+        classifier = BinaryRelevance(classifier=Keras(create_model_single_class, False, KERAS_PARAMS), require_dense=[True, True])
 
         self.assertClassifierWorksWithSparsity(classifier, 'dense')
 
@@ -48,7 +46,7 @@ class KerasTest(ClassifierBaseTest):
         self.assertClassifierWorksWithSparsity(classifier, 'sparse')
 
     def test_if_dense_classification_works_on_dense_base_classifier(self):
-        classifier = LabelPowerset(classifier=Keras(create_model_single_class, True, KERAS_PARAMS), require_dense=[True, True])
+        classifier = LabelPowerset(classifier=Keras(create_model_multiclass, True, KERAS_PARAMS), require_dense=[True, True])
 
         self.assertClassifierWorksWithSparsity(classifier, 'dense')
 
