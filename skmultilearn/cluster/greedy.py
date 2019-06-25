3f18c1b94fd74f2c8228b418a1e2c7de1422e4bb
Jose Perez-Parras Toledano <joseperezparras95@gmail.com>
2019-06-25 12:09:29 +0200

    Added Gakel

---
+++ skmultilearn/cluster/greedy.py
@@ -0,0 +1,171 @@
+from __future__ import absolute_import
+
+import random
+
+import numpy as np
+
+from .base import LabelSpaceClustererBase
+
+def label_correlation(y, s=0.35):
+    """Correlation between labels in a label matrix
+    Parameters
+    ----------
+    y : array-like (n_labels, n_samples)
+        Label matrix
+    s : float
+        Smoothness parameter
+    Returns
+    -------
+    L : array-like (n_labels, n_labels)
+        Label correlation matrix
+    """
+    L = np.zeros(shape=[y.shape[0], y.shape[0]])
+
+    for i in range(0, y.shape[0]):
+        yi = sum(y[i,:])
+        for j in range(0, y.shape[0]):
+            coincidence = 0
+            for k in range(0, y.shape[1]):
+                if (int(y[i,k]) == int(1)) and (int(y[j,k]) == int(1)):
+                    coincidence += 1
+            L[i,j] = (coincidence + s)/(yi + 2*s)
+
+return L
+
+class GreedyLabelSpaceClusterer(LabelSpaceClustererBase):
+    """Greedy divides the label space into equally-sized clusters
+
+    This method divides the label space by drawing without replacement a desired number of
+    equally sized subsets of label space, in a partitioning or overlapping scheme.
+
+    Parameters
+    ----------
+    cluster_size : int
+        desired size of a single cluster, will be automatically
+        put under :code:`self.cluster_size`.
+    cluster_count: int
+        number of clusters to divide into, will be automatically
+        put under :code:`self.cluster_count`.
+    allow_overlap : bool
+        whether to allow overlapping clusters or not, will be automatically
+        put under :code:`self.allow_overlap`.
+
+    Examples
+    --------
+
+    The following code performs greedy label space partitioning.
+
+    .. code :: python
+
+        from skmultilearn.cluster import GreedyLabelSpaceClusterer
+
+        # assume X,y contain the data, example y contains 5 labels
+        cluster_count = 2
+        cluster_size = y.shape[1]//cluster_count # == 2
+        clr = GreedyLabelSpaceClusterer(cluster_size, cluster_count, allow_overlap=False)
+        clr.fit_predict(X,y)
+        # Result:
+        # array([list([0, 4]), list([2, 3]), list([1])], dtype=object)
+
+
+    Note that the leftover labels that did not fit in `cluster_size` x `cluster_count` classifiers will be appended
+    to an additional last cluster of size at most `cluster_size` - 1.
+
+    You can also use this class to get a greedy division of the label space, even with multiple overlaps:
+
+    .. code :: python
+
+        from skmultilearn.cluster import GreedyLabelSpaceClusterer
+
+        cluster_size = 3
+        cluster_count = 5
+        clr = GreedyLabelSpaceClusterer(cluster_size, cluster_count, allow_overlap=True)
+        clr.fit_predict(X,y)
+
+        # Result
+        # array([[2, 1, 3],
+        #        [3, 0, 4],
+        #        [2, 3, 1],
+        #        [2, 3, 4],
+        #        [3, 4, 0],
+        #        [3, 0, 2]])
+
+
+    Note that you will never get the same label subset twice.
+    """
+
+    def __init__(self, cluster_size, cluster_count, allow_overlap):
+        super(GreedyLabelSpaceClusterer, self).__init__()
+
+        self.cluster_size = cluster_size
+        self.cluster_count = cluster_count
+        self.allow_overlap = allow_overlap
+
+    def fit_predict(self, X, y):
+        """Cluster the output space
+
+        Parameters
+        ----------
+        X : currently unused, left for scikit compatibility
+        y : scipy.sparse
+            label space of shape :code:`(n_samples, n_labels)`
+
+        Returns
+        -------
+        arrray of arrays of label indexes (numpy.ndarray)
+            label space division, each sublist represents labels that are in that community
+        """
+
+        if (self.cluster_count+1) * self.cluster_size < y.shape[1]:
+            raise ValueError("Cannot include all of {} labels in {} clusters of {} labels".format(
+                y.shape[1],
+                self.cluster_count,
+                self.cluster_size
+            ))
+
+        all_labels_assigned_to_division = False
+        # make sure the final label set division includes all labels
+        while not all_labels_assigned_to_division:
+            label_sets = []
+            free_labels = range(y.shape[1])
+
+            while len(label_sets) <= self.cluster_count:
+                if not self.allow_overlap:
+                    if len(free_labels) == 0:
+                        break
+
+                    # in this case, we are unable to draw new labels, add all that remain
+                    if len(free_labels) < self.cluster_size:
+                        label_sets.append(free_labels)
+                        break
+
+                #Changing the random way into a greedy way, it takes all of the labels by random sampling
+                #Takes a random label
+                    #Greedily interacts with each of the labels draw from the labelset
+                    #Creates the label_subset and then continues
+                L = label_correlation(np.transpose(y))
+                L = np.transpose(L)
+                #Get a random label
+                first_label = random.sample(free_labels, 1)
+                label_set = np.zeros(self.cluster_size)
+                label_set[0] = first_label
+                for i in range(1, len(label_set)):
+                    pass_next = False
+                    while not pass_next:
+                        next_label = np.argmax(L[label_set[,i-1])
+                        if next_label not in free_labels and next_label not in label_set:
+                            pass_next = True
+                    label_set[i] = next_label
+
+                if not self.allow_overlap:
+                    free_labels = list(set(free_labels).difference(set(label_set)))
+
+                if label_set not in label_sets:
+                    label_sets.append(label_set)
+
+            all_labels_assigned_to_division = all(
+                any(label in subset for subset in label_sets)
+                for label in range(y.shape[1])
+            )
+
+        return np.array(label_sets)
---
+++ skmultilearn/ensemble/gakeld.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+from .partition import LabelSpacePartitioningClassifier
+from ..cluster.greedy import GreedyLabelSpaceClusterer
+from ..problem_transform import LabelPowerset
+from ..base import MLClassifierBase
+
+
+class GakelD(MLClassifierBase):
+    """Distinct RAndom k-labELsets multi-label classifier.
+
+    Divides the label space in to equal partitions of size k, trains a Label Powerset
+    classifier per partition and predicts by summing the result of all trained classifiers.
+
+    With this implementation we will try a greedy approach for the partition of the labelset.
+
+    Parameters
+    ----------
+    base_classifier : sklearn.base
+        the base classifier that will be used in a class, will be
+        automatically put under :code:`self.classifier` for future
+        access.
+    base_classifier_require_dense : [bool, bool]
+        whether the base classifier requires [input, output] matrices
+        in dense representation, will be automatically
+        put under :code:`self.require_dense`
+    labelset_size : int
+        the desired size of each of the partitions, parameter k according to paper
+        Default is 3, according to paper it has the best results
+
+
+    Attributes
+    ----------
+    _label_count : int
+        the number of labels the classifier is fit to, set by :meth:`fit`
+
+    model_count_ : int
+        the number of sub classifiers trained, set by :meth:`fit`
+
+    classifier_: :class:`skmultilearn.ensemble.LabelSpacePartitioningClassifier`
+        the underneath classifier that perform the label space partitioning using a
+        random clusterer :class:`skmultilearn.ensemble.RandomLabelSpaceClusterer`
+
+
+    References
+    ----------
+
+    If you use this class please cite the paper introducing the method:
+
+    .. code :: latex
+
+        @ARTICLE{5567103,
+            author={G. Tsoumakas and I. Katakis and I. Vlahavas},
+            journal={IEEE Transactions on Knowledge and Data Engineering},
+            title={Random k-Labelsets for Multilabel Classification},
+            year={2011},
+            volume={23},
+            number={7},
+            pages={1079-1089},
+            doi={10.1109/TKDE.2010.164},
+            ISSN={1041-4347},
+            month={July},
+        }
+
+    Examples
+    --------
+
+    Here's a simple example of how to use this class with a base classifier from scikit-learn to teach
+    non-overlapping classifiers each trained on at most four labels:
+
+    .. code :: python
+
+        from sklearn.naive_bayes import GaussianNB
+        from skmultilearn.ensemble import GakelD
+
+        classifier = GakelD(
+            base_classifier=GaussianNB(),
+            base_classifier_require_dense=[True, True],
+            labelset_size=4
+        )
+
+        classifier.fit(X_train, y_train)
+        prediction = classifier.predict(X_test)
+
+    """
+
+    def __init__(self, base_classifier=None, labelset_size=3, base_classifier_require_dense=None):
+        super(GakelD, self).__init__()
+
+        self.labelset_size = labelset_size
+        self.base_classifier = base_classifier
+        self.base_classifier_require_dense = base_classifier_require_dense
+        self.copyable_attrs = ['base_classifier', 'base_classifier_require_dense', 'labelset_size']
+
+    def fit(self, X, y):
+        """Fit classifier to multi-label data
+
+        Parameters
+        ----------
+        X : numpy.ndarray or scipy.sparse
+            input features, can be a dense or sparse matrix of size
+            :code:`(n_samples, n_features)`
+        y : numpy.ndaarray or scipy.sparse {0,1}
+            binary indicator matrix with label assignments, shape
+            :code:`(n_samples, n_labels)`
+
+        Returns
+        -------
+        fitted instance of self
+        """
+        self._label_count = y.shape[1]
+        self.model_count_ = int(np.ceil(self._label_count / self.labelset_size))
+        self.classifier_ = LabelSpacePartitioningClassifier(
+            classifier=LabelPowerset(
+                classifier=self.base_classifier,
+                require_dense=self.base_classifier_require_dense
+            ),
+            clusterer=GreedyLabelSpaceClusterer(
+                cluster_size=self.labelset_size,
+                cluster_count=self.model_count_,
+                allow_overlap=False
+            ),
+            require_dense=[False, False]
+        )
+        return self.classifier_.fit(X, y)
+
+    def predict(self, X):
+        """Predict label assignments
+
+        Parameters
+        ----------
+        X : numpy.ndarray or scipy.sparse.csc_matrix
+            input features of shape :code:`(n_samples, n_features)`
+
+        Returns
+        -------
+        scipy.sparse of int
+            binary indicator matrix with label assignments with shape
+            :code:`(n_samples, n_labels)`
+        """
+
+        return self.classifier_.predict(X)
+
+    def predict_proba(self, X):
+        """Predict label probabilities
+
+        Parameters
+        ----------
+        X : numpy.ndarray or scipy.sparse.csc_matrix
+            input features of shape :code:`(n_samples, n_features)`
+
+        Returns
+        -------
+        scipy.sparse of float
+            binary indicator matrix with probability of label assignment with shape
+            :code:`(n_samples, n_labels)`
+        """
+
+        return self.classifier_.predict_proba(X)
