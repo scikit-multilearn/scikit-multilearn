5ae07aa4f5ae579921671e0c80ae71ba645fdace
Christian Schulze <accounts@andinfinity.de>
2023-03-14 13:49:31 +0100

    fix: fixed linting problems

--- skmultilearn/base/mock.py
+++ skmultilearn/base/mock.py
@@ -1,4 +1,6 @@
+import numpy as np
 from .base import MLClassifierBase
+from scipy.sparse import csr_matrix
 
 
 class MockClassifier(MLClassifierBase):
@@ -7,9 +9,9 @@ class MockClassifier(MLClassifierBase):
     def __init__(self):
         super(MockClassifier, self).__init__()
 
-    def fit(X, y):
+    def fit(self, X, y):
         self.label_count = y.shape[1]
         return self
 
-    def predict(X):
+    def predict(self, X):
         return csr_matrix(np.ones(shape=(X.shape[0], self.label_count), dtype=int))
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 from utils.files import files
 
+
 class Config:
     root = '.'
     meka = 'meka'
@@ -14,14 +15,18 @@ class Config:
 
 # #######################################################
 
+
 def test():
     return subprocess.call("py.test", shell=True)
 
+
 def clean():
     files.removeFilesRecursive(Config.meka, (lambda f: f.endswith('.pyc')))
-    files.removeFilesRecursive(Config.skmultilearn, (lambda f: f.endswith('.pyc')))
+    files.removeFilesRecursive(
+        Config.skmultilearn, (lambda f: f.endswith('.pyc')))
     files.removeFilesRecursive(Config.tests, (lambda f: f.endswith('.pyc')))
     files.removeFilesRecursive(Config.utils, (lambda f: f.endswith('.pyc')))
+
 
 def package():
     sourceDirs = [Config.meka, Config.skmultilearn, Config.tests, Config.utils]
@@ -30,8 +35,9 @@ def package():
             for dir in dirs:
                 initFile = os.path.join(root, dir, '__init__.py')
                 if not os.path.isfile(initFile):
-                    print 'creating : %s' % initFile
+                    print('creating : %s' % initFile)
                     open(initFile, 'a').close()
+
 
 def lint(full=False):
     from pylint import epylint
@@ -44,17 +50,20 @@ def lint(full=False):
 
     config = "--rcfile ./utils/pylint.config --msg-template=\"{C}:{msg_id}:{line:3d},{column:2d}:{msg}({symbol})\" -r %s %s"
     for dir in sources:
-        print 'lint %s' %dir
+        print('lint %s' % dir)
         epylint.py_run(config % (fullReport, dir), script='pylint')
+
 
 def lint_full():
     lint(True)
+
 
 def default():
     clean()
     package()
     lint()
-    #test()
+    # test()
+
 
 def install():
     try:
@@ -67,9 +76,11 @@ def install():
 
 ########################################################
 
+
 def step(msg):
     span = '=' * ((80 - len(msg)) / 2)
-    print ' '.join([span, msg, span])
+    print(' '.join([span, msg, span]))
+
 
 if __name__ == "__main__":
     os.environ['PYTHONPATH'] = os.path.dirname(os.path.realpath(__file__))
@@ -80,7 +91,7 @@ if __name__ == "__main__":
                 step(task)
                 locals()[task]()
             else:
-                print 'Error: task "%s" not found' % task
+                print('Error: task "%s" not found' % task)
                 sys.exit(1)
     else:
         default()
