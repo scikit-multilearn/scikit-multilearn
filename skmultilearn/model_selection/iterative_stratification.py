105aa8398b59c03a0227e8013dccc1264fd18cb7
Christopher O'Hagan <32102392+Christopher154@users.noreply.github.com>
2023-03-14 16:52:56 +0000

    Changed return order of the iterative_train_test_split (#180)
    Co-authored-by: ChristianSch <ChristianSch@users.noreply.github.com>

--- skmultilearn/model_selection/iterative_stratification.py
+++ skmultilearn/model_selection/iterative_stratification.py
@@ -104,7 +104,7 @@ def iterative_train_test_split(X, y, test_size, random_state=None):
     X_train, y_train = X[train_indexes, :], y[train_indexes, :]
     X_test, y_test = X[test_indexes, :], y[test_indexes, :]
 
-    return X_train, y_train, X_test, y_test
+    return X_train, X_test, y_train, y_test
 
 
 def _fold_tie_break(desired_samples_per_fold, M, random_state=check_random_state(None)):
